38f8fcb49e470fee2fcc53912c799328cdeb5223
Vitalii Podhornyi <57863039+JuniorDevMandarin@users.noreply.github.com>
2023-09-17 12:58:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+slownik = {
+    'Mama': 46545646456,
+    'Alex': 1231236,
+    'Mateusz': 13436456,
+    'Michal': 463453456456,
+}
 
+
+name = input('Podaj kontakt')
+if name in slownik.keys():
+    print(name, ':', slownik[name])
+else:
+    print('niema takiego kontaktu')
